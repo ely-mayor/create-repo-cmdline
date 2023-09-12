191dfdd275f98b6ba6d27f4f26ab47e65a526acb
Jerickson Mayor <jericksonmayor@gmail.com>
2023-09-13 07:33:56 +0800

    Enhanced create-repo.sh script and added description feature

--- create-repo.sh
+++ create-repo.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
+# Check if curl is installed
+if ! command -v curl &> /dev/null; then
+  echo "curl is not installed. Please install curl before running this script."
+  exit 1
+fi
+
+get_github_username() {
+  local response="$1"
+  local login_line=$(echo "$response" | grep '"login":')
+  local username=$(echo "$login_line" | cut -d'"' -f4)
+  echo "$username"
+}
+
+# Function to create or update repository description
+update_repo_description() {
+  local repo_owner="$1"
+  local repo_name="$2"
+  local token="$3"
+  echo "Enter a description for the repository:"
+  read DESCRIPTION
+
+  if [ -z "$DESCRIPTION" ]; then
+    echo "Description cannot be empty. No description added."
+  else
+    # Update the repository's description using the GitHub API
+    if curl -X PATCH \
+      -H "Authorization: token $token" -H "Accept: application/vnd.github.v3+json" \
+      "https://api.github.com/repos/$repo_owner/$repo_name" -d "{\"name\": \"$repo_name\", \"description\": \"$DESCRIPTION\"}" >/dev/null 2>&1; then
+      echo "Description updated successfully."
+    else
+      echo "Failed to update description on GitHub."
+    fi
+  fi
+}
+
 # Prompt for the repository name and ensure it's not empty
 while true; do
   echo "Enter repository name:"
@@ -60,11 +95,34 @@ while true; do
   fi
 done
 
-# Create the GitHub repository and redirect the curl response to /dev/null to hide it
+# Get the GitHub username programmatically and assign it to GITHUB_USERNAME
+API_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user)
+GITHUB_USERNAME=$(get_github_username "$API_RESPONSE")
+
+# Create the GitHub repository and handle errors
 if curl -f -X POST \
   -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/user/repos -d "{\"name\": \"$REPO_NAME\", \"private\": $REPO_PRIVATE}" >/dev/null 2>&1; then
   echo "Repository '$REPO_NAME' created successfully on GitHub."
+
+  # Ask the user if they want to add a description or about section
+  while true; do
+    echo "Do you want to add a description to the repository? (Y/N):"
+    read ADD_DESCRIPTION
+
+    ADD_DESCRIPTION=$(echo "$ADD_DESCRIPTION" | tr '[:lower:]' '[:upper:]')
+
+    if [ "$ADD_DESCRIPTION" == "Y" ] || [ "$ADD_DESCRIPTION" == "YES" ]; then
+      update_repo_description "$GITHUB_USERNAME" "$REPO_NAME" "$GITHUB_TOKEN"
+      break
+    elif [ "$ADD_DESCRIPTION" == "N" ] || [ "$ADD_DESCRIPTION" == "NO" ]; then
+      echo "No description added."
+      break
+    else
+      echo "Invalid input. Please enter 'Y' for yes or 'N' for no."
+    fi
+  done
+
 else
   echo "Failed to create repository on GitHub."
 fi
